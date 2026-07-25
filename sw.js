fdaa7e67c14a86377cfa6a71120ab4deec2bac8e
saram-tech <githubsaram@gmail.com>
2026-07-25 00:26:34 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
         puis supprimera automatiquement les caches obsolètes.
    ===================================================================== */
 
-const CACHE_VERSION = 'v2';
+const CACHE_VERSION = 'v3';
 const CACHE_NAME = 'boulliwel-pro-' + CACHE_VERSION;
 
 // Fichiers constituant l'app shell : nécessaires au fonctionnement hors ligne
